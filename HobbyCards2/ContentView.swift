80a8640b3fbe833c9183b653a9bfc154790708d8
Jack Rathwell <jrathwell@lcs.on.ca>
2025-02-08 11:35:12 -0500

    Initial Commit

---
+++ HobbyCards2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HobbyCards2
+//
+//  Created by Jack William Rathwell on 2025-02-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HobbyCards2/HobbyCards2App.swift
@@ -0,0 +1,17 @@
+//
+//  HobbyCards2App.swift
+//  HobbyCards2
+//
+//  Created by Jack William Rathwell on 2025-02-08.
+//
+
+import SwiftUI
+
+@main
+struct HobbyCards2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
