b3817921770b6c3edf85aeb58cc789951e4cf641
Jack Rathwell <jrathwell@lcs.on.ca>
2025-02-13 11:05:27 -0500

    made custom shape

--- HobbyCards2/Shape/Star.swift
+++ HobbyCards2/Shape/Star.swift
@@ -7,9 +7,33 @@
 
 import SwiftUI
 
-struct Star: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct Star: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        
+        var path = Path()
+        
+        //define custom shape
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        //line 1
+        path.addLine(to: CGPoint(x: 0.65*rect.maxX, y: 0.37*rect.maxY))
+        //line 2
+        path.addLine(to: CGPoint(x: rect.maxX, y: 0.75*rect.midY))
+        //line 3
+        path.addLine(to: CGPoint(x: 0.75*rect.maxX, y: rect.midY))
+        //line 4
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        //line 5
+        path.addLine(to: CGPoint(x: rect.midX, y: 0.75*rect.maxY))
+        //line 6
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        //line 7
+        path.addLine(to: CGPoint(x: 0.25*rect.maxX, y: rect.midY))
+        //line 8
+        path.addLine(to: CGPoint(x: rect.minX, y: 0.37*rect.maxY))
+        //line 9
+        path.addLine(to: CGPoint(x: 0.33*rect.maxX, y: 0.37*rect.maxY))
+        return path
     }
 }
 
